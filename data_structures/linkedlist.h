c9490b359799a74be41a7bc8e817e008c8768e03
Bohemian-Ophidian <Karan_whistleblower@protonmail.com>
2026-03-12 15:35:43 +0530

    improving visibility

---
+++ data_structures/linkedlist.h
@@ -0,0 +1,23 @@
+#include <stdbool.h>
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+typedef struct Node{
+        int data;//remove this 
+        struct Node* next;//remove this
+}Node;
+
+bool isEmpty(Node* head);
+void display(Node* head);
+
+int len(Node* head);
+
+Node* add(Node* head, int data);
+Node* addEnd(Node* head, int data);
+int getData(Node* head);
+Node* removeHead(Node* head);
+Node* removeTail(Node* head);
+Node* destroy(Node* head);
+Node* reverse(Node* head);
+Node* next(Node* head);
+#endif
